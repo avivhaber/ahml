774b0c5f1883173109236296d548486bd05e869d
Aviv Haber <avivh72@gmail.com>
2023-06-25 15:14:31 -0400

    bug

--- engine.py
+++ engine.py
@@ -1,25 +1,67 @@
+import math
+
 class Scalar:
     @classmethod
     def wrap(cls, obj):
         return obj if isinstance(obj, Scalar) else Scalar(obj)
 
-    def __init__(self, val, op=None, left=None, right=None):
+    def __init__(self, val, op=None, children=(), childgrads=()):
         self.val = val
         self.op = op
-        self.left = left
-        self.right = right
+        self.dim = len(children)
+        self.children = children
+        self._childgrads = childgrads
+        self.grad = 0
+
+    def _backward_from(self, acc):
+        for child, cgrad in zip(self.children, self._childgrads):
+            child.grad += acc * cgrad
+            child._backward_from(acc * cgrad)
+
+    # Compute derivative of this scalar wrt all dependent variables
+    def backward(self):
+        self.grad = 1
+        self._backward_from(1)
 
     def __add__(self, other):
         other = Scalar.wrap(other)
-        return Scalar(self.val + other.val, '+', self, other)
+        return Scalar(self.val + other.val, '+', (self,other), (1,1))
     
     def __mul__(self, other):
         other = Scalar.wrap(other)
-        return Scalar(self.val * other.val, '*', self, other)
+        dleft = other.val
+        dright = self.val
+        return Scalar(self.val * other.val, '*', (self,other), (dleft,dright))
     
     def __pow__(self, other):
-        other = Scalar.wrap(other)
-        return Scalar(self.val ** other.val, '^', self, other)
+        assert isinstance(other, (int, float))
+        d = other * self.val**(other - 1)
+        return Scalar(self.val ** other, '^', (self,), (d,))
+    
+    def relu(self):
+        d = 0 if self.val <= 0 else 1
+        return Scalar(max(0, self.val), 'r', (self,), (d,))
     
     def __repr__(self):
         return str(self.val)
+    
+    def __neg__(self):
+        return self * -1
+
+    def __radd__(self, other):
+        return self + other
+
+    def __sub__(self, other):
+        return self + (-other)
+
+    def __rsub__(self, other):
+        return other + (-self)
+
+    def __rmul__(self, other):
+        return self * other
+
+    def __truediv__(self, other):
+        return self * other**-1
+
+    def __rtruediv__(self, other):
+        return other * self**-1
--- test.py
+++ test.py
@@ -1,4 +1,17 @@
 from engine import Scalar
-a = Scalar(7) + 1
-b = a * 5 + Scalar(3)
-print(b)
+a = Scalar(-4.0)
+b = Scalar(2.0)
+c = a + b
+d = a * b + b**3
+c += c + 1
+c += 1 + c + (-a)
+d += d * 2 + (b + a).relu()
+d += 3 * d + (b - a).relu()
+e = c - d
+f = e**2
+g = f / 2.0
+g += 10.0 / f
+print(g)
+g.backward()
+print(a.grad)
+print(b.grad)
